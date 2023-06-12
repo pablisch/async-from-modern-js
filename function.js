a1a35d4f29361970f94cae9b9582b2e9d385f084
Pablo <pablito@me.com>
2023-06-12 14:39:57 +0100

    Added files to function section

---
+++ function.js
@@ -0,0 +1,17 @@
+// const toggle = (e) => {
+//   e.target.classList.toggle('danger');
+// }
+
+// function toggle(e) {
+//   e.target.classList.toggle('danger');
+// }
+
+document.querySelector('button').addEventListener('click', toggle);
+
+const toggle = (e) => {
+  e.target.classList.toggle('danger');
+}
+
+function toggle(e) {
+  e.target.classList.toggle('danger');
+}
